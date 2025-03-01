75c944d951c83d341117e01989a70054ce8b663e
Ваше Имя <your.email@example.com>
2025-03-01 19:45:28 +0700

    Исправлены ошибки PEP-8

--- converter.py
+++ converter.py
@@ -1,4 +1,10 @@
-def c_to_f(celsius):return (celsius * 9/5) + 32
-def f_to_c(fahrenheit): return (fahrenheit - 32) * 5/9
+def c_to_f(celsius):
+    return (celsius * 9 / 5) + 32
+
+
+def f_to_c(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
+
+
 print("100°C в Фаренгейтах:", c_to_f(100))
 print("212°F в Цельсиях:", f_to_c(212))
